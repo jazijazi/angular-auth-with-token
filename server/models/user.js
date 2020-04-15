c35565acc36435cf4eb07ea97dd04a3cd70c4c71
Jazi <jazi1374@gmail.com>
2020-04-16 00:37:40 +0430

    delete tracking f

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-//create user table(model) in database :
-const mongoose = require('mongoose') ; 
-
-const Schema = mongoose.Schema ;
-const userSchema = new Schema ({
-    email : String ,
-    password : String
-});
-module.exports = mongoose.model('user', userSchema, 'users') //mongoose.model(name , Schema , collection)
